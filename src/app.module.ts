794ee257dfad9cd41c587d8aea509aadf76940dd
Janine Erika Marie M. Balangue <janine@smartworksintl.com>
2023-06-02 15:09:26 +0800

    Added Sample ENV file and reconfigure mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { BooksModule } from './books/books.module';
   imports: [
     ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MDB_USER}:${process.env.MDB_PASSWORD}@$teethtalk-app.uhu5edb.mongodb.net/${process.env.MDB_CLUSTER}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MDB_USER}:${process.env.MDB_PASSWORD}@teethtalk-app.uhu5edb.mongodb.net/${process.env.MDB_CLUSTER}?retryWrites=true&w=majority`,
     ),
     BooksModule,
   ],
